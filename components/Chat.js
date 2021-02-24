e6a2daf3fe1bf1077508a6138610eafa1ba32250
zukui <71074389+zukui1984@users.noreply.github.com>
2021-02-24 22:30:39 +0100

    add keyboard & giftedChart at chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,30 +1,92 @@
-import React from 'react';
-import { Text, View, Button } from "react-native";
+import React from "react";
+import {
+  Text,
+  View,
+  Button,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { name: "" };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "This is a system message",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#c6bff7",
+          },
+        }}
+      />
+    );
+  }
 
   render() {
     let name = this.props.route.params.name;
-    let color = this.props.route.params.color || 'red'; 
+    let color = this.props.route.params.color || "lightgrey";
     this.props.navigation.setOptions({ title: name });
 
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", 
-      backgroundColor: color }}>
-        {/* <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={(name) => this.setState({ name })}
-          value={this.state.name}
-          placeholder="Type here ..."
-        /> */}
-        {/* <Text>You wrote: {this.state.text}</Text> */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: color,
+        }}
+      >
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{ _id: 1 }}
+        />
+
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
+
         <Button
-        title="Go to Start"
-          onPress={() => this.props.navigation.navigate ("Start")} />      
+          title="Go to Start"
+          onPress={() => this.props.navigation.navigate("Start")}
+        />
       </View>
     );
   }
